41ceeb9a3884b160594faad867da0fe9b23a281c
Brian Gerkens <67236902+GitCoding123@users.noreply.github.com>
2023-11-08 13:30:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+import * as vari from "./js/variable.js";
+import { add } from "./js/functions/js"; 
+
+console.log(vari.num1);
+console.log(vari.num2);
+
+const sum = add(vari.num1, vari.num2);
+
+console.log(sum);
